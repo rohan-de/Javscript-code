e61a4c5912443502fb687a39a28fc6ed2b714e97
Rohan <149697636+rohan-de@users.noreply.github.com>
2026-07-05 15:32:55 +0000

    config

---
+++ basic/variables.js
@@ -0,0 +1,7 @@
+const accountID = 145985
+let accountEmail = "rohan2546@google.com"
+var accountPassword = 76657
+accountCity = "kolkata"
+
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
